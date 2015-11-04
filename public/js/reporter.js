d32c71deda76ce15ebf28190940c560948502af9
Andrew <andrew@aramirez2-mac.home>
2015-11-04 00:16:05 -0500

    automation reporting

--- public/js/reporter.js
+++ public/js/reporter.js
@@ -1,10 +1,17 @@
 $(function () {
     var Reporter = {};
-    var $json = $('#results').data('value');
-    var $body = $('body');
+    var $json = $('#results').data('value'),
+        $body = $('body'),
+        $resultSummary = $("#results-summary"),
+        twoHundred = 0,
+        twoHundredOne = 0,
+        twoHundredFour = 0,
+        fiveHundred = 0,
+        fourHundredFour = 0,
+        latencyAverage = [];
+
 
     var buildTable = function () {
-        console.log($json);
         $body.append("<table id='result-table'></table>");
         $("#result-table").append(
             "<thead>" +
@@ -19,11 +26,12 @@ $(function () {
     };
 
     var addRows = function() {
-      var table = $('#result-table');
+      var $table = $('#result-table');
         $.each($json, function(i, item) {
             var jsonObj = JSON.parse($json[i]);
-            console.log(jsonObj.latency);
-            table.append(
+            latencyAverage.push(jsonObj.latency);
+            parseResponseCodes(jsonObj.responseCode);
+            $table.append(
                 "<tr>" +
                     "<td>"+jsonObj.requestType+"</td>" +
                     "<td>"+jsonObj.responseCode+"</td>" +
@@ -34,19 +42,78 @@ $(function () {
         })
     };
 
-    var createAveragebar = function() {
+    var createNavBar = function() {
+        $body.prepend(
+            '<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">' +
+                '<div class="container"> ' +
+                    '<div class="navbar-header">' +
+                        '<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">' +
+                            '<span class="sr-only">Toggle navigation</span>' +
+                            '<span class="icon-bar"></span>' +
+                            '<span class="icon-bar"></span>' +
+                            '<span class="icon-bar"></span>' +
+                        '</button>' +
+                        '<a class="navbar-brand" href="#">Test Results</a>' +
+                    '</div>' +
+                '</div>' +
+            '</div>'
+            )
+    };
 
+    var createAveragebar = function() {
+        $resultSummary.append(
+            "<div id='summary'>" +
+                "<ul id='summary-list'>" +
+                    "<li><span style='color:green; padding-right:8px;'>200</span>"+ twoHundred +"</li>" +
+                    "<li><span style='color:green; padding-right:8px'>201</span>"+ twoHundredOne +"</li>" +
+                    "<li><span style='color:darkred; padding-right:8px'>204</span>"+ twoHundredFour +"</li>" +
+                    "<li><span style='color:red; padding-right:8px'>500</span>"+ fiveHundred +"</li>" +
+                    "<li><span style='color:grey; padding-right:8px'>404</span>"+ fourHundredFour +"</li>" +
+                    "<li><span style='color:cadetblue; padding-right:8px'>latency</span>"+ averageLatency() +"</li>" +
+            "</div>");
+    };
+
+    var parseResponseCodes = function(responseCode) {
+        if (responseCode == 200) {
+            twoHundred = twoHundred + 1;
+        } else if (responseCode == 201) {
+            twoHundredOne = twoHundredOne +1;
+        } else if (responseCode == 500) {
+            fiveHundred = fiveHundred +1;
+        } else if (responseCode == 404) {
+            fourHundredFour = fourHundredFour + 1;
+        } else {
+            console.log("unknown responseCode: " + responseCode);
+        }
+    };
+
+    var averageLatency = function() {
+        var entries = latencyAverage.length;
+        var total = 0;
+        $.each(latencyAverage,function() {
+            total += this;
+        });
+        console.log("Full latency average: " + total / entries);
+        return(Math.floor(total / entries));
     };
 
     var defineDataTable= function() {
         $("#result-table").DataTable();
     };
 
-    var init = function () {
+    var buildUI = function() {
+        createNavBar();
         buildTable();
         defineDataTable();
+        createAveragebar();
+    };
+
+
+    var init = function () {
+        buildUI();
     };
 
     init();
+
     window.Reporter = Reporter;
 });
